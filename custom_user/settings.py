8aa4239e83c6717b5e051942c26c555ce99f24f8
DIOMANDE-CODE <56660103+DIOMANDE-CODE@users.noreply.github.com>
2020-12-22 00:15:59 +0000

    Update settings.py

--- custom_user/settings.py
+++ custom_user/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&57j63w_6zz8q2(ashn=vu=al$is2t#y9hpe@%80@)8oi$q+nd'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
